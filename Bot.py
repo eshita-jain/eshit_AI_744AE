6aaf6ffebe4ccf51c0d3c35ad935b2a9195de8b6
Aniket Kumar <79148315+k-aniket47@users.noreply.github.com>
2022-11-12 22:16:45 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,34 +1,53 @@
+
 import random
 from time import sleep
 
-EMPTY = 0
+box = 0
+empty_space = 0
+
 class player:
     def __init__(self):
         self.step=0
 
     def move(self,B,N,cur_x,cur_y):
-        self.step+=1
-        if B[(cur_x+1)%N][cur_y]==0:
-            return (1,0)
+        global box
 
-        if B[(cur_x+N-1)%N][cur_y]==0:
-            return (-1,0)
-            
-        if B[cur_x][(cur_y+1)%N]==0:
-            return (0,1)
+        step_size = B[cur_x][cur_y]^3
 
-        if B[cur_x][(cur_y+N-1)%N]==0:
-            return (0,-1)
-        
-        
+        if box == 0:
+            for i in range(0, N):
+                for j in range(0, N):
+                    if B[i][j] == step_size:
+                        box += 1
 
-        return self.closest_empty(B,N,cur_x,cur_y) 
-    def closest_empty(self,B,N,cur_x,cur_y):
+        if box == 1:
+            box = 0
+            return self.find_next(B,N,cur_x,cur_y)
+
+        elif box == 2:
+            self.step+=1
+            if B[(cur_x+1)%N][cur_y]==0:
+                return (1,0)
+
+            if B[(cur_x+N-1)%N][cur_y]==0:
+                return (-1,0)
+                
+            if B[cur_x][(cur_y+1)%N]==0:
+                return (0,1)
+
+            if B[cur_x][(cur_y+N-1)%N]==0:
+                return (0,-1)
+
+            return self.find_next(B,N,cur_x,cur_y) 
+
+
+    def find_next(self,B,N,cur_x,cur_y):
+        step_size=B[cur_x][cur_y]^3
         dis=2*N+1
         best = {"x":cur_x,"y":cur_y}
         for i in range(N):
             for j in range(N):
-                if B[i][j] == EMPTY:
+                if B[i][j] == empty_space:
                     dx = min ( abs(cur_x - i) , N - abs(cur_x - i) )
                     dy = min ( abs(cur_y - j) , N - abs(cur_y - j) )
                     cur_dis = dx+dy
@@ -56,16 +75,11 @@ class player:
             else:
                 return (0,-1)
 
-
         if best["y"] < cur_y:
             if cur_y-best["y"] < N/2:
                 return (0,-1)
             else:
                 return (0,1)
-
-
-        
-        
 
         return (0,0)
 
